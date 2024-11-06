859af3a261c9fc07f6a52b74fd50db52f40dffbd
S SAI TANUSH <saitanush789@gmail.com>
2024-11-06 19:29:22 +0530

    chore: added logic to check whether the user logged in isStudent or not to access the student route

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -38,8 +38,25 @@ exports.auth = async (req, res, next) => {
   }
 };
 
+//isStudent
+exports.isStudent = async (req, res, next) => {
+  try {
+    if (req.user.accontType !== "Student") {
+      return res.status(401).json({
+        success: false,
+        message: "This is a protected route for Students only",
+      });
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "User role cannot be verified, please try again ",
+    });
+  }
+};
+
 //isAdmin
 
 //isInstructor
-
-//isStudent
