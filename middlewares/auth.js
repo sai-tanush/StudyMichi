ea5c218aa5f53a8bc3a1828ec9affad94ffa5621
S SAI TANUSH <saitanush789@gmail.com>
2024-11-06 19:21:45 +0530

    chore: added jwt token verification logic for user authentication and authorization

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,45 @@
+const jwt = require("jsonwebtoken");
+require("dotenv");
+const User = require("../models/User");
+
 //auth
+exports.auth = async (req, res, next) => {
+  try {
+    //extract token
+    const token =
+      req.cookies.token ||
+      req.body.token ||
+      req.header("Authorisation").replace("Bearer ", "");
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Token is missing",
+      });
+    }
+
+    //verify the token
+    try {
+      const decode = jwt.verify(token, process.env.JWT_SECRET);
+      console.log(decode);
+      req.user(decode);
+    } catch (error) {
+      return res.status(401).json({
+        success: false,
+        message: "token is invalid",
+      });
+    }
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Something went wrong while validating token",
+    });
+  }
+};
 
 //isAdmin
 
 //isInstructor
 
 //isStudent
-
-
-
-
