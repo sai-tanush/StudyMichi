ce94c6c8b69d64c18018dccae93ed77681d3fb8b
S SAI TANUSH <saitanush789@gmail.com>
2024-11-06 17:45:18 +0530

    chore: added otp-generation logic with DB checking for unique OTP

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,7 +1,67 @@
-//auth
+const user = require("../models/User");
+const OTP = require("../models/");
+const otpGenerator = require("otp-generator");
 
-//isAdmin
+//send OTP
+exports.sendOtp = async (req, res) => {
+  try {
+    //fetch email from request.body
+    const { email, body } = req.body;
 
-//isInstructor
+    //check if user already exists
+    const checkUserPresent = await User.findOne({ email });
 
-//isStudent
+    //if user already exists , then return a response
+    if (checkUserPresent) {
+      return res.status(401).json({
+        success: false,
+        message: "User already registered",
+      });
+    }
+
+    //generate OTP
+    let otp = otpGenerator.generate(6, {
+      upperCaseAlphabets: false,
+      lowerCaseAlphabets: false,
+      specialChars: false,
+    });
+    console.log("OTP generated successfully! = ", otp);
+
+    //check if otp is unique or not
+    const result = await OTP.findOne({ otp: otp });
+
+    while (result) {
+      otp = otpGenerator.generate(6, {
+        upperCaseAlphabets: false,
+        lowerCaseAlphabets: false,
+        specialChars: false,
+      });
+      result = await OTP.findOne({ otp: otp });
+    }
+
+    const otpPayload = { email, otp };
+
+    //create entry for OTP in DB
+    const otpBody = await OTP.create(otpPayload);
+    console.log(otpBody);
+
+    //return successfull response
+    res.status(200).json({
+        success: true,
+        message: 'OTP Sent Successfully!',
+        otp,
+    })
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+        success: false,
+        message: error.message, 
+    })
+  }
+
+  //isAdmin
+
+  //isInstructor
+
+  //isStudent
+};
