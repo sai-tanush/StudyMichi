4644835fe6388a90baa0b1d6384bba2d80820596
S SAI TANUSH <saitanush789@gmail.com>
2024-11-06 22:05:49 +0530

    chore: added logic to resetPassword Token

--- controllers/ResetPassword.js
+++ controllers/ResetPassword.js
@@ -1,1 +1,57 @@
-//forgot-Password
+const User = require("../models/User");
+const mailSender = require("../utils/mailSender");
+
+//resetPasswordToken
+exports.resetPasswordToken = async (req, res) => {
+  try {
+    //get email from req.body
+    const { email } = req.body;
+
+    //check user exists or not
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(403).json({
+        success: false,
+        message: "User not registered with us",
+      });
+    }
+
+    //generate token
+    const token = crypto.randomUUID();
+
+    //update user by adding token and expiration time
+    const updatedDetails = await User.findOneAndUpdate(
+      { email: email },
+      {
+        token: token,
+        resetPasswordExpires: Date.now() + 5 * 60 * 1000,
+      },
+      {
+        new: true,
+      }
+    );
+
+    //create url
+    const url = `http://localhost:3000/update-password/${token}`;
+
+    //send mail containing the url
+    await mailSender(
+      email,
+      "Password Reset Link",
+      `Password Reset Link: ${url} `
+    );
+    //return response
+
+    return res.status(200).json({
+      success: true,
+      message:
+        "Email Sent successfully, Please check email and change your password",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Password cannot be reset",
+    });
+  }
+};
