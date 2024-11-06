bb14a4c967cde826957da0872b35a0e6c12809ea
S SAI TANUSH <saitanush789@gmail.com>
2024-11-06 18:41:22 +0530

    chore: added user login logic with email and password validation & additonalyy added jwt token as cookie when successful login

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -2,6 +2,8 @@ const User = require("../models/User");
 const OTP = require("../models/");
 const otpGenerator = require("otp-generator");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 //send OTP
 exports.sendOtp = async (req, res) => {
@@ -165,3 +167,66 @@ exports.sendOtp = async (req, res) => {
     }
   };
 };
+
+//login
+exports,
+  (logn = async (req, res) => {
+    try {
+      //fetch data from request.body
+      const { email, password } = request.body;
+
+      //validate data
+      if (!email || !password) {
+        return res.status(403).json({
+          success: false,
+          message: "All fields are necessary! Please Retry",
+        });
+      }
+
+      //check if user exists or not
+      const user = await User.findOne({ email }).populate("additionalDetails");
+      if (!user) {
+        return res.status(401).json({
+          success: false,
+          message: "User is not registered!, please register first!",
+        });
+      }
+
+      //generate JWT, after password matching
+      if (await bcrypt.compare(password, user.password)) {
+        const payload = {
+          email: user.email,
+          id: user._id,
+          role: user.role,
+        };
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+          expiresIn: "2h",
+        });
+        user.token = token;
+        user.password = undefined;
+
+        //create cookie and send response
+        const options = {
+          expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+          httpOnly: true,
+        };
+        res.cookie("token", token, options).status(200).json({
+          success: true,
+          token,
+          user,
+          message: "User Logged in successfully!",
+        });
+      } else {
+        return res.status(401).json({
+          success: false,
+          message: "Passwords is incorrect",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        success: false,
+        message: "Login failure, Please try again!",
+      });
+    }
+  });
