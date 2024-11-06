8f372d11f342c2f990185cb2fe27f969e3be3f0b
S SAI TANUSH <saitanush789@gmail.com>
2024-11-06 12:57:58 +0530

    chore: added config file to connect mongodb

---
+++ config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+exports.connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("DB connected successfully!");
+  } catch (err) {
+    console.log("DB connection failed!");
+    console.error(err);
+    process.exit(1);
+  }
+};
