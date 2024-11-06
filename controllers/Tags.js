8323f711253df19d4de45ac43344b8dfa13e587d
S SAI TANUSH <saitanush789@gmail.com>
2024-11-06 22:42:54 +0530

    chore: added handler function to create Tags

---
+++ controllers/Tags.js
@@ -0,0 +1,36 @@
+const tag = require("../models/Tags");
+
+//Create handler function for tags
+exports.createTag = async (req, res) => {
+  try {
+    //fetch data
+    const { name, description } = req.body;
+
+    //validation
+    if (!name || !description) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    //create entry in DB
+    const tagDetails = await Tag.create({
+      name: name,
+      description: description,
+    });
+    console.log(tagDetails);
+
+    //return response
+    return res.status(200).json({
+      success: true,
+      message: "Tag created successfully!",
+    });
+  } catch (error) {
+    console.log(error);
+    return res(500).json({
+      success: false,
+      message: "Error while creating tags",
+    });
+  }
+};
