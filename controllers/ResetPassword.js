d0082d2c3bac1ca131c2b9787357fa4ba4a23645
S SAI TANUSH <saitanush789@gmail.com>
2024-11-06 22:21:36 +0530

    chore: added logic to resetPassword & validate token

--- controllers/ResetPassword.js
+++ controllers/ResetPassword.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const mailSender = require("../utils/mailSender");
+const bcrypt = require("bcrypt");
 
 //resetPasswordToken
 exports.resetPasswordToken = async (req, res) => {
@@ -52,6 +53,94 @@ exports.resetPasswordToken = async (req, res) => {
     return res.status(500).json({
       success: false,
       message: "Password cannot be reset",
+    });
+  }
+};
+
+//resetPassword
+exports.resetPassword = async (req, res) => {
+  try {
+    //fetch data
+    const { password, confirmPassword, token } = req.body;
+
+    //validation
+    if (password !== confirmPassword) {
+      return res.status(403).json({
+        success: false,
+        message: "Password not matching!",
+      });
+    }
+
+    //get userdetails from db using token
+    const userDetails = await User.findOne({ token: token });
+
+    //Check if token is invalid
+    if (!userDetails) {
+      return res.status(400).json({
+        success: false,
+        message: "Token is invalid",
+      });
+    }
+
+    //check for token expiration time
+    if (userDetails.resetPasswordExpires < Date.now()) {
+      return res.status(400).json({
+        success: false,
+        message: "Token is expired, please regenerate your token",
+      });
+    }
+
+    //hash password
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    //update password
+    await User.findOneAndUpdate(
+      { token: token },
+      { password: hashPassword },
+      { new: true }
+    );
+
+    //return response
+    res.status(200).json({
+      success: true,
+      message: "Password changed successfully!",
+    });
+    //Check if token is invalid
+    if (!userDetails) {
+      return res.status(400).json({
+        success: false,
+        message: "Token is invalid",
+      });
+    }
+
+    //check for token expiration time
+    if (userDetails.resetPasswordExpires < Date.now()) {
+      return res.status(400).json({
+        success: false,
+        message: "Token is expired, please regenerate your token",
+      });
+    }
+
+    //hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    //update password
+    await User.findOneAndUpdate(
+      { token: token },
+      { password: hashedPassword },
+      { new: true }
+    );
+
+    //return response
+    res.status(200).json({
+      success: true,
+      message: "Password changed successfully!",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Error while Password reset",
     });
   }
 };
