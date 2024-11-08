a81ffc552a01499fae05995a2eac00cdec5247aa
S SAI TANUSH <saitanush789@gmail.com>
2024-11-08 18:14:03 +0530

    chore: added handler function to update a subSection

--- controllers/SubSection.js
+++ controllers/SubSection.js
@@ -60,6 +60,56 @@ exports.createSubsection = async (req, res) => {
   }
 };
 
+//update a subSection
+exports.updateSubSection = async (req, res) => {
+  try {
+    //fetch data
+    const { subSectionId } = req.params;
+    const { title, timeDuration, description } = req.body;
+
+    //validate data
+    if (!subSectionId) {
+      return res.status(400).json({
+        success: false,
+        message: "SubSection ID is required!",
+      });
+    }
+
+    if (!title || !timeDuration || !description) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are necessary!",
+      });
+    }
+
+    //update data
+    const updateSubSection = await SubSection.findByIdAndUpdate(
+      subSectionId,
+      { title, timeDuration, description },
+      { new: true }
+    );
+
+    if (!updatedSubSection) {
+      return res.status(404).json({
+        success: false,
+        message: "SubSection not found!",
+      });
+    }
+
+    //return resposne
+    return res.status(200).json({
+      success: true,
+      message: "SubSection updated successfully!",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error while updating a subsection",
+      error: error.message,
+    });
+  }
+};
+
 //delete a subSection
 exports.deleteSubSection = async (req, res) => {
   try {
