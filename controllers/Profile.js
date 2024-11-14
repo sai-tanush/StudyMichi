e338a562a191c60f43a988880e11a6004558590e
S SAI TANUSH <saitanush789@gmail.com>
2024-11-14 16:22:12 +0530

    refactor: updated updateProfile handler function

--- controllers/Profile.js
+++ controllers/Profile.js
@@ -3,46 +3,34 @@ const User = require("../models/User");
 
 //update Profile
 exports.updateProfile = async (req, res) => {
-  try {
-    //fetch data
-    const { dateOfBirth = "", about = "", contactNumber, gender } = req.body;
+	try {
+		const { dateOfBirth = "", about = "", contactNumber } = req.body;
+		const id = req.user.id;
 
-    //get userId
-    const id = req.user.id;
+		// Find the profile by id
+		const userDetails = await User.findById(id);
+		const profile = await Profile.findById(userDetails.additionalDetails);
 
-    //validate data
-    if (!contactNumber || !gender || !id) {
-      return res.status(400).json({
-        success: false,
-        message: "All fields are required!",
-      });
-    }
+		// Update the profile fields
+		profile.dateOfBirth = dateOfBirth;
+		profile.about = about;
+		profile.contactNumber = contactNumber;
 
-    //find profile
-    const userDetails = await User.findById(id);
-    const profileId = userDetails.additionalDetails;
-    const profileDetails = await Profile.findById(profileId);
+		// Save the updated profile
+		await profile.save();
 
-    //update profile
-    profileDetails.dateOfBirth = dateOfBirth;
-    profileDetails.about = about;
-    profileDetails.gender = gender;
-    profileDetails.contactNumber = contactNumber;
-    await profileDetails.save();
-
-    //return response
-    return res.status(200).json({
-      success: true,
-      message: "Profile updated successfully!",
-      profileDetails,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      success: false,
-      message: "Error while updating Profile",
-      error: error.message,
-    });
-  }
+		return res.json({
+			success: true,
+			message: "Profile updated successfully",
+			profile,
+		});
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({
+			success: false,
+			error: error.message,
+		});
+	}
 };
 
 //delete Account
