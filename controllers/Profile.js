1f46d445283c59fe6b2cb429a66d85a8ca87fbc5
S SAI TANUSH <saitanush789@gmail.com>
2024-11-17 21:26:03 +0530

    chore: added updateProfilePicture handler function to update profile picture of user

--- controllers/Profile.js
+++ controllers/Profile.js
@@ -1,5 +1,6 @@
 const Profile = require("../models/Profile");
 const User = require("../models/User");
+require("dotenv").config();
 
 //update Profile
 exports.updateProfile = async (req, res) => {
@@ -97,3 +98,34 @@ exports.getAllUserDetails = async (req, res) => {
     });
   }
 };
+
+//update displayPicture of user
+exports.updateDisplayPicture = async (req, res) => {
+    try {
+      const displayPicture = req.files.displayPicture
+      const userId = req.user.id
+      const image = await uploadImageToCloudinary(
+        displayPicture,
+        process.env.FOLDER_NAME,
+        1000,
+        1000
+      )
+      console.log(image)
+      const updatedProfile = await User.findByIdAndUpdate(
+        { _id: userId },
+        { image: image.secure_url },
+        { new: true }
+      )
+      res.send({
+        success: true,
+        message: `Image Updated successfully`,
+        data: updatedProfile,
+      })
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: error.message,
+      })
+    }
+};
+  
