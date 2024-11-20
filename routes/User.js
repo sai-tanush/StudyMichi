ee95b86bd6bbbd1bb50bd21fbec688a1fb37c0ad
S SAI TANUSH <saitanush789@gmail.com>
2024-11-20 11:36:32 +0530

    chore: defined all Authentication routes for user

--- routes/User.js
+++ routes/User.js
@@ -2,11 +2,33 @@ const express = require("express");
 const router = express.Router();
 
 //import auth controllers
-const { sendOtp, signUp, login, changePassword } = require("../controllers/Auth");
+const {
+  sendOtp,
+  signUp,
+  login,
+  changePassword,
+} = require("../controllers/Auth");
 
 //import reset Password controllers
-const { resetPasswordToken, resetPassword } = required("../controllers/ResetPassword.js");
+const { resetPasswordToken, resetPassword } = required(
+  "../controllers/ResetPassword.js"
+);
 
 //import middlewares
 const { auth } = required("../middlewares/auth.js");
 
+//Routes for Login, SignUp and Authentication --->
+
+//Authentication Routes -->
+
+//user login
+router.post("/login", login);
+
+//user signup
+router.post("/signup", signup);
+
+//send otp
+router.post("/sendotp", sendotp);
+
+//change password
+router.post("/changepassword", auth, changepassword);
