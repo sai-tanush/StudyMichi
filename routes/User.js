be3bb74b21dcb9ca7f97469e9fa4bc5c8527cae4
S SAI TANUSH <saitanush789@gmail.com>
2024-12-03 17:40:00 +0530

    refactor: removed auth checking from reset-password-token as it is not required

--- routes/User.js
+++ routes/User.js
@@ -34,7 +34,7 @@ router.post("/sendotp", sendOtp);
 //Reset-Password -->
 
 //change password
-router.post("/reset-password-token", auth, resetPasswordToken);
+router.post("/reset-password-token", resetPasswordToken);
 
 //reset-password
 router.post("/reset-password", resetPassword);
