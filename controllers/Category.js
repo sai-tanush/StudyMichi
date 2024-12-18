463bebcae084c5f9fb65b8e19e24d0bbaa264ea3
S SAI TANUSH <saitanush789@gmail.com>
2024-12-18 19:51:28 +0530

    chore: added categoryPageDetails handler function

--- controllers/Category.js
+++ controllers/Category.js
@@ -65,11 +65,23 @@ exports.categoryPageDetails = async (req, res) => {
     //fetch categoryId from req.body
     const { categoryId } = req.body;
 
+    if(!categoryId){
+      return res.status(400).json({
+          success: false,
+          message: "categoryId is required"
+      })
+    };
+
     // Get courses for the specified category
     const selectedCategory = await Category.findById(categoryId)
-      .populate("courses")
-      .exec();
-    console.log(selectedCategory);
+        .populate({
+          path: "courses",
+          match: { status: "Published" },
+          populate: "ratingAndReviews",
+        })
+        .exec()
+
+    console.log("selectedCategory in categoryPageDetails = ", selectedCategory);
 
     // Handle the case when the category is not found
     if (!selectedCategory) {
@@ -88,31 +100,47 @@ exports.categoryPageDetails = async (req, res) => {
       });
     }
 
-    const selectedCourses = selectedCategory.courses;
-
     // Get courses for other categories
     const categoriesExceptSelected = await Category.find({
       _id: { $ne: categoryId },
-    }).populate("courses");
-    let differentCourses = [];
-    for (const category of categoriesExceptSelected) {
-      differentCourses.push(...category.courses);
-    }
+    })
+    let differentCategory = await Category.findOne(
+      categoriesExceptSelected[getRandomInt(categoriesExceptSelected.length)]
+        ._id
+    )
+      .populate({
+        path: "courses",
+        match: { status: "Published" },
+      })
+      .exec()
+
+      console.log("Different COURSE", differentCategory)
 
     // Get top-selling courses across all categories
-    const allCategories = await Category.find().populate("courses");
+    const allCategories = await Category.find()
+    .populate({
+      path: "courses",
+      match: { status: "Published" },
+      populate: {
+        path: "instructor"
+      },
+    })
+    .exec();
+
     const allCourses = allCategories.flatMap((category) => category.courses);
     const mostSellingCourses = allCourses
       .sort((a, b) => b.sold - a.sold)
       .slice(0, 10);
 
+    console.log("mostSellingCourses COURSE", mostSellingCourses);
+    
     //return response
     res.status(200).json({
       success: true,
       data: {
-        selectedCourses: selectedCourses,
-        differentCourses: differentCourses,
-        mostSellingCourses: mostSellingCourses,
+        selectedCourses,
+        differentCourses,
+        mostSellingCourses,
       },
     });
   } catch (error) {
